15ec53dac0b4d531c148fdb30621a4d12c94c49f
bustlezach <bustlezach001@gmail.com>
2023-10-23 10:58:21 -0700

    task

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,4 +1,4 @@
-import $ from 'jquery';
+import $ from "jquery";
 import _ from 'lodash';
 
 $('body').append('<p>Holberton Dashboard</p>');
@@ -7,18 +7,12 @@ $('body').append('<button>Click here to get started</button>');
 $('body').append("<p id='count'></p>");
 $('body').append('<p>Copyright - Holberton School</p>');
 
-let counter = 0;
 
-// const btn = document.querySelector('#count');
-// function updateCounter() {
-//   counter += 1;
-//   btn.innerHTML = `${counter} clicks on the button`;
-// };
-// btn.addEventListener('click', _.debounce(updateCounter, 500));
+let count = 0;
 
 function updateCounter() {
-  counter += 1;
-  $('#count').text(`${counter} clicks on the button`);
-};
+  count += 1;
+  $('#count').text(`${count} clicks on the button`);
+}
 
 $('button').on('click', _.debounce(updateCounter, 500));
