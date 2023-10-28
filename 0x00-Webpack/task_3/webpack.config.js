588a0254016a8eb555bbe9e700d7d9d193847a4a
OLUWASEUN OLORUNSHOLA <bustlezach001@gmail.com>
2023-10-28 17:53:20 +0100

    task

--- 0x00-Webpack/task_3/webpack.config.js
+++ 0x00-Webpack/task_3/webpack.config.js
@@ -3,43 +3,50 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  mode: 'production',
-  // entry: './js/dashboard_main.js',
+  mode: 'development',
   entry: {
     all: ["./modules/header/header.js", "./modules/body/body.js", "./modules/footer/footer.js"],
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public')
   },
   performance: {
     maxAssetSize: 1000000,
     maxEntrypointSize: 1000000,
   },
-  plugins: [
-    new CleanWebpackPlugin(), new HtmlWebpackPlugin(),
-  ],
-  devServer: {
-    port: 8564,
-    compress: true,
-    contentBase: path.join(__dirname, 'public'),
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "public")
   },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, './public'),
+    compress: true,
+    port: 8564,
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin(),
+  ],
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        test: /\.css$/i,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
       },
       {
-        test: /\.(jpeg|jpg|gif|png|svg)$/,
-        use: ['file-loader', { 
-          loader: 'image-webpack-loader',
-          options: {
-            bypassOnDebug: true,
-            disable: true,
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true,
+              disable: true,
+            },
           },
-        }],
+        ],
       },
     ]
-  }
+  },
 };
